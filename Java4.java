0ed7bbf0e46a64fd77f2290d948788b807e7c8d5
AsmaSadiya <syedasma7777@gmail.com>
2021-11-25 04:03:13 +0000

    my first commit

---
+++ Java4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Java3
+ {
+  public static void main(String[] args)  
+  {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter an integer:");
+      int n=sc.nextInt();
+      Scanner as=new Scanner(System.in);
+      System.out.println("enter an double:");
+      Double a=as.nextDouble();
+      Scanner am=new Scanner(System.in);
+      System.out.println("enter an string:");
+      String e=am.nextLine();
+    
+
+    }
+
+}
+
+OUTPUT:
+
+enter an integer:
+3
+enter an double:
+3.3435
+enter an string:
+iamasma
